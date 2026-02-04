dd4e1603a95f5c8aef733e2dbbdc2ae96581d758
davlet61 <dovlat93@gmail.com>
2026-02-04 16:33:19 +0100

    ui/ux enhancements

--- scripts/tmux-fzf-switch.sh
+++ scripts/tmux-fzf-switch.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
+SCRIPT_PATH="$(realpath "$0")"
+
 FZF_COLORS="bg:#282c34,fg:#abb2bf,hl:#61afef,fg+:#e5c07b,bg+:#414858,hl+:#61afef,prompt:#98c379,pointer:#c678dd,marker:#e06c75,header:#5c6370,border:#495162"
 
-VIM_KEYS="j,k,g,G,q,/,i,ctrl-d,ctrl-u,0,1,2,3,4,5,6,7,8,9"
+VIM_KEYS="j,k,g,G,q,/,i,x,?,ctrl-d,ctrl-u,0,1,2,3,4,5,6,7,8,9"
+
+# List generator — used for initial display and fzf reload after kill
+if [ "$1" = "--list" ]; then
+  case "$2" in
+  windows)
+    tmux list-windows -F $'#{window_index}\t#{?window_active,* ,  }#{window_name} #{pane_current_path} (#{window_panes} panes)'
+    ;;
+  panes)
+    tmux list-panes -s -F $'#{window_index}.#{pane_index}\t#{?#{&&:#{window_active},#{pane_active}},* ,  }#{window_name} #{pane_current_path}'
+    ;;
+  all)
+    tmux list-panes -a -F $'#{session_name}:#{window_index}.#{pane_index}\t#{?#{&&:#{window_active},#{pane_active}},* ,  }#{window_name} [#{session_name}] #{pane_current_path}'
+    ;;
+  esac | sed -e "s|$HOME|~|g" -e $'s/\t\\* /\t\x1b[32m* /' -e $'s/$/\x1b[0m/'
+  exit 0
+fi
 
 FZF_OPTS=(
   --ansi
@@ -12,9 +30,12 @@ FZF_OPTS=(
   --color "$FZF_COLORS"
   --no-info
   --delimiter '\t'
+  --with-nth '2..'
   --disabled
   --prompt '  '
+  --header $'j/k:nav  g/G:ends  ^d/u:page\n/:search  0-9:jump  x:kill  q/esc:quit'
   --header-first
+  --bind "start:toggle-header"
   --bind "j:down,k:up,g:first,G:last,ctrl-d:half-page-down,ctrl-u:half-page-up"
   --bind "enter:accept"
   --bind "q:abort"
@@ -22,6 +43,7 @@ FZF_OPTS=(
   --bind "i:unbind($VIM_KEYS)+enable-search+clear-query+change-prompt(/ )"
   --bind "esc:transform:[[ \$FZF_PROMPT == '/ ' ]] && echo 'disable-search+rebind($VIM_KEYS)+clear-query+change-prompt(  )' || echo 'abort'"
   --bind "change:transform-query:[[ \$FZF_PROMPT == '/ ' ]] && echo \$FZF_QUERY || echo ''"
+  --bind "?:toggle-header"
 )
 
 # Number keys enter search mode with the digit pre-filled
@@ -31,18 +53,18 @@ done
 
 case "$1" in
 windows)
-  selected=$(tmux list-windows -F $'#{window_index}\t#{window_name} #{pane_current_path} (#{window_panes} panes)' |
+  selected=$("$SCRIPT_PATH" --list windows |
     fzf "${FZF_OPTS[@]}" \
-      --header 'j/k:nav  /:search  q:quit' \
-      --preview 'tmux capture-pane -ep -t :{1}')
+      --preview 'tmux capture-pane -ep -t :{1}' \
+      --bind "x:execute-silent(tmux kill-window -t :{1})+reload($SCRIPT_PATH --list windows)")
   [ -n "$selected" ] && tmux select-window -t ":${selected%%	*}"
   ;;
 
 panes)
-  selected=$(tmux list-panes -s -F $'#{window_index}.#{pane_index}\t#{window_name} #{pane_current_path}' |
+  selected=$("$SCRIPT_PATH" --list panes |
     fzf "${FZF_OPTS[@]}" \
-      --header 'j/k:nav  /:search  q:quit' \
-      --preview 'tmux capture-pane -ep -t :{1}')
+      --preview 'tmux capture-pane -ep -t :{1}' \
+      --bind "x:execute-silent(tmux kill-pane -t :{1})+reload($SCRIPT_PATH --list panes)")
   if [ -n "$selected" ]; then
     target="${selected%%	*}"
     tmux select-window -t ":${target%%.*}"
@@ -51,10 +73,10 @@ panes)
   ;;
 
 all)
-  selected=$(tmux list-panes -a -F $'#{session_name}:#{window_index}.#{pane_index}\t#{window_name} [#{session_name}] #{pane_current_path}' |
+  selected=$("$SCRIPT_PATH" --list all |
     fzf "${FZF_OPTS[@]}" \
-      --header 'j/k:nav  /:search  q:quit' \
-      --preview 'tmux capture-pane -ep -t {1}')
+      --preview 'tmux capture-pane -ep -t {1}' \
+      --bind "x:execute-silent(tmux kill-pane -t {1})+reload($SCRIPT_PATH --list all)")
   [ -n "$selected" ] && tmux switch-client -t "${selected%%	*}"
   ;;
 
